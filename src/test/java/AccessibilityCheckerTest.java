2a7a50ce6b4ceb1886c16d146ceca876ccbdcb50
Andrei Florea <andrei.c.florea63@gmail.com>
2023-12-07 00:20:47 +0200

    Added comments

--- src/test/java/AccessibilityCheckerTest.java
+++ src/test/java/AccessibilityCheckerTest.java
@@ -25,25 +25,26 @@ public class AccessibilityCheckerTest {
     private static int rowNumberFirst = 0;
     private static int rowNumberLast = 1;
     private static int rowNumber = 2;
-    private final String[] header = {"URL", "Name", "Impact", "Count", "HTML Target"};
+    private final String[] header = {"URL", "Name", "Impact", "Count", "HTML Target"}; // Header columns
     private final XSSFWorkbook workbook = new XSSFWorkbook();
-    private final Sheet sheet = workbook.createSheet("Accessibility report");
+    private final Sheet sheet = workbook.createSheet("Accessibility report"); // Creates the sheet named "Accessibility report"
     private final Row headerRow = sheet.createRow(0);
-    private final String defaultUrl = getProperty("url") + getProperty("lang");
-    private final String loginPagePath = "/user/login";
+    private final String defaultUrl = getProperty("url") + getProperty("lang"); // Creates the URL, taking into consideration the lang as well
+    private final String loginPagePath = "/user/login"; // Path (optional) to be used in case tests need to be run with a logged-in user
     static ChromeOptions options = new ChromeOptions();
-    private String fileName = "Accessibility Report <Project> ";
+    private static String fileName = "Accessibility Report <Project> "; // File name to be used for the report, final file name will be: Accessibility Report <Project> dd.MM.yyyy
+    private static String propertiesFileName = "accessibility"; // Properties file name to be used for the tests
 
     @BeforeAll
-    public static void setupDriver() {
+    public static void setupDriver() { // Adds arguments into the driver
         //"--headless"
         options.addArguments();
         driver = new ChromeDriver(options);
     }
 
-    static WebDriver driver;
+    static WebDriver driver; // Initializes the driver
     @AfterAll
-    public static void quitDriver() {
+    public static void quitDriver() { // Closes the driver after execution
         if (driver != null) {
             driver.quit();
         }
@@ -58,8 +59,8 @@ public class AccessibilityCheckerTest {
 
         createExcelSheetAndHeader();
 
-        for (String userType : userTypeList){
-            switch (userType){
+        for (String userType : userTypeList) { // Iterates through the list of user types
+            switch (userType) { // Different steps for different user types
                 case "userType1":
                     testPath(userType);
                     break;
@@ -72,7 +73,7 @@ public class AccessibilityCheckerTest {
         writeExcelFile();
     }
 
-    public void userLogin(String username, String password) {
+    public void userLogin(String username, String password) { // Login method in case pages need to be tested from a logged-in user perspective as well
         driver.navigate().to(defaultUrl + loginPagePath);
         WebElement usernameField = driver.findElement(By.id("edit-name"));
         WebElement passwordField = driver.findElement(By.id("edit-pass"));
@@ -80,12 +81,12 @@ public class AccessibilityCheckerTest {
         passwordField.sendKeys(password + Keys.ENTER);
     }
 
-    public static String getProperty(String propertyName) {
+    public static String getProperty(String propertyName) { // Method to retrieve properties from .properties file
         try {
             Properties properties = new Properties();
-            String filePath = "src/test/resources/accessibility.properties";
+            propertiesFileName = "src/test/resources/" + propertiesFileName + ".properties";
 
-            FileInputStream fileInputStream = new FileInputStream(filePath);
+            FileInputStream fileInputStream = new FileInputStream(propertiesFileName);
             properties.load(fileInputStream);
 
             return properties.getProperty(propertyName);
@@ -95,45 +96,45 @@ public class AccessibilityCheckerTest {
         return null;
     }
 
-    public void testPath (String typeOfUser) {
+    public void testPath (String typeOfUser) { // Method to test all the paths from paths.userType
         try {
             String[] pathsList = (getProperty("paths." + typeOfUser)).split("\\s*,\\s*");
-            for (String path : pathsList){
-                String url = defaultUrl + path;
+            for (String path : pathsList){ // Iterates through the list of paths
+                String url = defaultUrl + path; // Creates the url with path to navigate to
                 driver.navigate().to(url);
-                JSONObject response = new AXE.Builder(driver, scriptUrl).analyze();
-                JSONArray violations = response.getJSONArray("violations");
-                if (violations.length() > 0) {
-                    rowNumberFirst = rowNumberLast+1;
-                    rowNumberLast = rowNumberLast + violations.length();
+                JSONObject response = new AXE.Builder(driver, scriptUrl).analyze(); // Returns the analyzed web page as a JSONObject response
+                JSONArray violations = response.getJSONArray("violations"); // Returns only the violations from the response
+                if (violations.length() > 0) { // Checks if the number of violations is greater than 0
+                    rowNumberFirst = rowNumberLast+1; // Creates the first row number for the violations
+                    rowNumberLast = rowNumberLast + violations.length(); // Creates the last row number for the creations
                     for (int i = 0; i < violations.length(); i++) {
 
                         JSONObject violation = violations.getJSONObject(i);
-                        String URL = driver.getCurrentUrl();
-                        String name = violation.getString("help");
+                        String URL = driver.getCurrentUrl(); // Tested URL
+                        String name = violation.getString("help"); // Name of the violation
 
                         JSONArray parentNode = violation.getJSONArray("nodes");
                         JSONObject childNode = parentNode.getJSONObject(0);
-                        String impact = childNode.getString("impact");
+                        String impact = childNode.getString("impact"); // Impact level of the violation
                         int count = 0;
-                        String[] htmlTargets = new String[50];
+                        String[] htmlTargets = new String[50]; // Creates a String array, size set to 50
                         StringBuilder htmlTarget = new StringBuilder();
-                        for (int j = 0; j < parentNode.length(); j++) {
+                        for (int j = 0; j < parentNode.length(); j++) { // Iterates through the nodes of "nodes" node
                             JSONObject node = parentNode.getJSONObject(j);
-                            if (node.has("html")){
-                                htmlTargets[j] = node.getString("html");
-                                count+=1;
-                                if (count == parentNode.length()-1) {
+                            if (node.has("html")){ // Checks if the node has a "html" key
+                                htmlTargets[j] = node.getString("html"); // Creates the StringBuilder that contains the html element
+                                count+=1; // Counter of the keys "html" inside "nodes" mpde
+                                if (count == parentNode.length()-1) { // Adds "linebreakhere" only if the count is not equal to the first to penultimate StringBuilder variable
                                     htmlTarget.append(htmlTargets[j]).append(" linebreakhere ");
                                 }
                                 else
                                     htmlTarget.append(htmlTargets[j]);
                             }
                         }
-                        writeExcelRow(rowNumber, URL, name, impact, count, htmlTarget);
+                        writeExcelRow(rowNumber, URL, name, impact, count, htmlTarget); // Writes a single Excel row with the gathered informations
                         rowNumber++;
                     }
-                    mergeURLCells(rowNumberFirst, rowNumberLast);
+                    mergeURLCells(rowNumberFirst, rowNumberLast); // Merges cells for the current "violation" node when URL is the same
                 }
             }
         } catch (Exception ignored) {
@@ -141,13 +142,14 @@ public class AccessibilityCheckerTest {
     }
 
     public void createExcelSheetAndHeader() {
-        //Creating the header
+        // Creates the header style, with bold text
         CellStyle headerStyle = sheet.getWorkbook().createCellStyle();
         Font font = sheet.getWorkbook().createFont();
         font.setBold(true);
         headerStyle.setFont(font);
         headerStyle.setBorderBottom(BorderStyle.MEDIUM);
 
+        // Creates the header with the header style
         for (int i = 0; i < header.length; i++){
             Cell cell = headerRow.createCell(i);
             cell.setCellStyle(headerStyle);
@@ -156,7 +158,7 @@ public class AccessibilityCheckerTest {
     }
 
     public void writeExcelRow(int rowNumber, String URL, String name, String impact, int count, StringBuilder target) {
-        //Creating data rows
+        // Creates data rows
         Row dataRow = sheet.createRow(rowNumber-1);
 
         Cell URLCell = dataRow.createCell(0);
@@ -179,7 +181,7 @@ public class AccessibilityCheckerTest {
         countCell.setCellStyle(createAlignCenterStyle());
         countCell.setCellValue(count);
 
-
+        // Creates a String variable from StringBuilder, with each String on a separate cell row
         String targetInputString = target.toString();
         String[] substrings = targetInputString.split("linebreakhere");
         String targetToBeWritten = String.join("\n", substrings);
@@ -187,12 +189,14 @@ public class AccessibilityCheckerTest {
         targetCell.setCellStyle(createAlignCenterStyle());
         targetCell.setCellValue(targetToBeWritten);
 
+        // Accesses the setBorders method only if it's the last row of the violations found on the particular page
         if (rowNumber == rowNumberLast) {
             setBorders(URLCell, nameCell, impactCell, countCell, targetCell);
         }
     }
 
     public void setBorders(Cell URLCell, Cell nameCell, Cell impactCell, Cell countCell, Cell targetCell) {
+        // Sets thin borders after the last cell of the violations found on the particular page
         URLCell.setCellStyle(createBottomThinBorderStyle());
         nameCell.setCellStyle(createBottomThinBorderStyle());
         impactCell.setCellStyle(createBottomThinBorderStyle());
@@ -201,35 +205,38 @@ public class AccessibilityCheckerTest {
     }
 
     public void mergeURLCells(int firstRow, int lastRow) {
+        // Merges a range of cells
         CellRangeAddress cellMerge = new CellRangeAddress(firstRow-1, lastRow-1, 0, 0);
         sheet.addMergedRegion(cellMerge);
     }
 
     private CellStyle createAlignCenterStyle() {
+        // Creates a CellStyle with style vertical alignment
         CellStyle style = workbook.createCellStyle();
         style.setVerticalAlignment(VerticalAlignment.CENTER);
         return style;
     }
 
     private CellStyle createBottomThinBorderStyle() {
+        // Creates a CellStyle with style bottom thin border
         CellStyle style = workbook.createCellStyle();
         style.setBorderBottom(BorderStyle.THIN);
         return style;
     }
     
     public void writeExcelFile() {
-        //Auto-size all columns
+        // Auto-size all columns, commented because it doesn't work as expected
         /*for (int i = 0; i < header.length; i++){
             System.out.println("Column: " + i + " has been resized");
             sheet.autoSizeColumn(i);
         }*/
 
-        //Set file name
+        // Sets file name with the current date
         LocalDate today = LocalDate.now();
         String formattedDate = today.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         fileName = fileName + formattedDate;
 
-        //Write Excel file
+        // Writes Excel file
         try {
             FileOutputStream out = new FileOutputStream("./" + fileName +".xlsx");
             workbook.write(out);
